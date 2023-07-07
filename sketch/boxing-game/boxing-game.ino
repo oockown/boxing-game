09daf711f4e4880ebbf48253051e12fdb553db14
oockown <oockown@gmail.com>
2023-07-07 23:00:53 +0700

    initial commit

---
+++ sketch/boxing-game/boxing-game.ino
@@ -0,0 +1,25 @@
+/* game life cycle :
+ *  - start button pushed
+ *  - release target, start sound, display goes zero
+ *  - target being hit
+ *  - count time different, punched sound, display start count and put score
+ *  - if higher than HS, replace high score display, sound clapping, else sound wooing
+ *  - display still high score and last score
+ *  - wait for another pushed start button
+ *  
+ *  input :   - start button
+ *            - punch sensor (magnet or vibration)
+ *  output :  - p10 panel display
+ *            - mini DF Player
+ *            - 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
